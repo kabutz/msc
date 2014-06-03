6bb8b0866095c13306d4b855c913d4634327e24a
Dr Heinz M. Kabutz <kabutz@javaspecialists.eu>
2014-06-03 22:18:05 +0300

    Loaded UnsafeProvider in boot class path

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #java -Xbootclasspath/p:classpath OverheadExperiment $*
-java -cp bootclasspath:src/classpath OverheadExperiment $*
+#java -cp bootclasspath:src/classpath OverheadExperiment $*
+java -Xbootclasspath/p:bootclasspath -cp src/classpath OverheadExperiment $*
